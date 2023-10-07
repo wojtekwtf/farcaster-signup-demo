0c15572116f15b8a33b2ce835930557a07ec73c3
wojtekwtf <wojciechkulikowski@icloud.com>
2023-10-07 11:50:38 +0100

    Configure optimism

--- src/app/web3Provider.tsx
+++ src/app/web3Provider.tsx
@@ -2,20 +2,23 @@
 
 import { FC, PropsWithChildren } from 'react'
 import { WagmiConfig, createConfig } from 'wagmi'
+import { optimism } from 'wagmi/chains'
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
 
 const config = createConfig(
   getDefaultConfig({
     // Required API Keys
+    alchemyId: "wdTgzo6QiXtOb9HI8z-uqNaVfZ1PCLCP",
     walletConnectProjectId: "5a2cb35e0ed7f091a5c2c9a5cf4ed988",
+    chains: [optimism],
 
     // Required
     appName: "Sign up for Farcaster",
 
     // Optional
-    appDescription: "Your App Description",
-    appUrl: "https://family.co", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+    appDescription: "Simple app illustrating how to sing up Farcaster. Educational purposes only.",
+    appUrl: "https://www.farcaster.xyz/", // your app's url
+    appIcon: "https://framerusercontent.com/modules/jVMp8b8ZfTZpbLnhDiml/NV8p4XHr9GEQFJDJsKKb/assets/DE2CvWySqIW7eDC8Ehs5bCK6g.svg", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   }),
 );
 
