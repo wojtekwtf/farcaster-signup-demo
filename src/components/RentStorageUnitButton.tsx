bf66108495972a4106fdb39435651c8773cde1a5
wojtekwtf <wojciechkulikowski@icloud.com>
2023-10-09 13:17:49 +0100

    Add error handling to rent button

--- src/components/RentStorageUnitButton.tsx
+++ src/components/RentStorageUnitButton.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react'
 
 import { CheckCircleIcon } from '@heroicons/react/24/outline'
 import PuffLoader from "react-spinners/PuffLoader";
+import { toast } from 'sonner'
 
 export default function RegisterFIDButton() {
 
@@ -13,14 +14,14 @@ export default function RegisterFIDButton() {
   const { isConnected } = useAccount()
   const [price, setPrice] = useState<number>(0)
 
-  const { data: unitPrice } = useContractRead({
-    // address: '0x00000000fcce7f938e7ae6d3c335bd6a1a7c593d', // mainnet
-    address: '0xa6B79d91FAD0E4952FDaB8Cc2DE803fC423aAdBf', // testnet
+  const { data: unitPrice, isError: isErrorPriceRead, error: errorPriceRead } = useContractRead({
+    address: '0x00000000fcce7f938e7ae6d3c335bd6a1a7c593d', // mainnet
+    // address: '0xa6B79d91FAD0E4952FDaB8Cc2DE803fC423aAdBf', // testnet
     abi: StorageRegistryABI,
     functionName: 'unitPrice',
   })
 
-  const { config } = usePrepareContractWrite({
+  const { config, isError: isErrorWrite, error: errorWrite } = usePrepareContractWrite({
     address: '0x00000000fcce7f938e7ae6d3c335bd6a1a7c593d', // mainnet
     // address: '0xa6B79d91FAD0E4952FDaB8Cc2DE803fC423aAdBf', // testnet
     abi: StorageRegistryABI,
@@ -35,6 +36,17 @@ export default function RegisterFIDButton() {
     hash: rentTxHash?.hash,
   })
 
+  const rentStorageUnit = async () => {
+    if (isErrorPriceRead) {
+      toast.error("Cannot rent a storage unit", { description: errorPriceRead?.message })
+    }
+    if (isErrorWrite) {
+      toast.error("Cannot rent a storage unit", { description: errorWrite?.message })
+    }
+
+    write?.()
+  }
+
   useEffect(() => {
     if (unitPrice) {
       setPrice(Math.floor(Number(unitPrice) * 1.1))
@@ -45,7 +57,7 @@ export default function RegisterFIDButton() {
 
     <button
       disabled={!isConnected || !fid}
-      onClick={() => write?.()}
+      onClick={() => rentStorageUnit()}
       type="button"
       className={`w-28 inline-flex justify-center items-center gap-x-2 rounded-md bg-purple-600 disabled:bg-purple-200 px-3 py-2 text-sm font-semibold text-white shadow-sm disabled:shadow-none disabled:cursor-not-allowed hover:bg-purple-500 duration-100 dark:disabled:bg-purple-900 dark:disabled:bg-opacity-60 dark:disabled:text-gray-300 ${isSuccess && '!bg-green-500 !text-white'}}`}
     >
