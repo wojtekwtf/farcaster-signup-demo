88f3ddc1ed166274bea762104407e394a0a4282b
wojtekwtf <wojciechkulikowski@icloud.com>
2023-10-14 15:04:04 -0400

    Update hub ip

--- src/app/hub/route.ts
+++ src/app/hub/route.ts
@@ -1,6 +1,6 @@
 import { getInsecureHubRpcClient, Message } from "@farcaster/hub-nodejs";
 
-const HUB_URL = "3.137.157.154:2283"; // URL + Port of the Hub
+const HUB_URL = "3.17.4.160:2283"; // URL + Port of the Hub
 
 const client = getInsecureHubRpcClient(HUB_URL);
 
@@ -8,6 +8,8 @@ export async function POST(req: Request) {
     const data = await req.json();
     const message = Message.fromJSON(data.message)
     const result = await client.submitMessage(message)
+
+    console.log(result)
 
     if (result.isErr()) {
         return new Response(result.error.message, { status: 400 });
