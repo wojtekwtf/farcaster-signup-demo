8b0cc69875ec7650e955658c8400b6b94cc47645
horsefacts <webmaster@terminally.online>
2023-11-27 13:28:45 -0500

    Fix typos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import ClientLayout from '../providers/web3Provider'
-import { FidProvider } from '../providers/fidContext'
-import { SignerProvider } from '@/providers/signerContext'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ClientLayout from "../providers/web3Provider";
+import { FidProvider } from "../providers/fidContext";
+import { SignerProvider } from "@/providers/signerContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Sign up for Farcaster',
-  description: 'Simple app illustrating how to sing up Farcaster. Educational purposes only.',
-}
+  title: "Sign up for Farcaster",
+  description:
+    "Simple app illustrating how to sign up for Farcaster. Educational purposes only.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <link rel="icon" href="https://framerusercontent.com/modules/jVMp8b8ZfTZpbLnhDiml/NV8p4XHr9GEQFJDJsKKb/assets/DE2CvWySqIW7eDC8Ehs5bCK6g.svg"></link>
+      <link
+        rel="icon"
+        href="https://framerusercontent.com/modules/jVMp8b8ZfTZpbLnhDiml/NV8p4XHr9GEQFJDJsKKb/assets/DE2CvWySqIW7eDC8Ehs5bCK6g.svg"
+      ></link>
       <body className={inter.className}>
         <ClientLayout>
           <FidProvider>
-            <SignerProvider>
-              {children}
-            </SignerProvider>
+            <SignerProvider>{children}</SignerProvider>
           </FidProvider>
         </ClientLayout>
       </body>
     </html>
-  )
+  );
 }
--- src/providers/web3Provider.tsx
+++ src/providers/web3Provider.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import { FC, PropsWithChildren } from 'react'
-import { WagmiConfig, createConfig, mainnet } from 'wagmi'
-import { optimism, optimismGoerli } from 'wagmi/chains'
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+import { FC, PropsWithChildren } from "react";
+import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { optimism, optimismGoerli } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
 const config = createConfig(
   getDefaultConfig({
@@ -17,16 +17,18 @@ const config = createConfig(
     appName: "Sign up for Farcaster",
 
     // Optional
-    appDescription: "Simple app illustrating how to sing up Farcaster. Educational purposes only.",
+    appDescription:
+      "Simple app illustrating how to sign up for Farcaster. Educational purposes only.",
     appUrl: "https://www.farcaster.xyz/", // your app's url
-    appIcon: "https://framerusercontent.com/modules/jVMp8b8ZfTZpbLnhDiml/NV8p4XHr9GEQFJDJsKKb/assets/DE2CvWySqIW7eDC8Ehs5bCK6g.svg", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  }),
+    appIcon:
+      "https://framerusercontent.com/modules/jVMp8b8ZfTZpbLnhDiml/NV8p4XHr9GEQFJDJsKKb/assets/DE2CvWySqIW7eDC8Ehs5bCK6g.svg", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
 );
 
 const Web3Provider: FC<PropsWithChildren<{}>> = ({ children }) => (
   <WagmiConfig config={config}>
     <ConnectKitProvider>{children}</ConnectKitProvider>
   </WagmiConfig>
-)
+);
 
-export default Web3Provider
+export default Web3Provider;
